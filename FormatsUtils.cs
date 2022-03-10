7c4d65f6b31686d21b80e66f2aa5e3d63d6ff8e3
Srg <usikov90@mail.ru>
2022-03-10 17:20:42 +0300

    FormatUtils

---
+++ FormatsUtils.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RevitAPIUILibrary
+{
+    public class FormatsUtils
+    {
+        public static List<string> GetFormat(ExternalCommandData commandData)
+        {
+            var Value = new List<string>();
+            string[] format = new string[4] { "DWG", "IFC", "NWC", "JPG" };
+            
+            foreach (string i in format)
+            {
+                Value.Add(i);
+            }
+            return Value;
+        }        
+    }   
+}
